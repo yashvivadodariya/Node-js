7b407fa8402b5358b4e3c35f51d7ab51f4929f73
yashvivadodariya <yashvivadodariya72@gmail.com>
2026-03-17 12:49:35 +0530

    PR_8Removed

--- PR-8/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dbConnect = () => {
-    mongoose.connect('mongodb+srv://Yashvi:yashvi%40123@cluster0.uooaon0.mongodb.net/pr-7')
-        .then(() => console.log("DB is connected"))
-        .catch((err) => console.log(err));
-}
-
-module.exports = dbConnect;
